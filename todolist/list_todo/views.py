53e1a1f7c8003c6171abb342be53bb6f2f922d3a
Hanieh Khalesi <khalesihanieh@gmail.com>
2024-09-03 01:17:27 +0330

    clean code

--- todolist/list_todo/views.py
+++ todolist/list_todo/views.py
@@ -1,16 +1,14 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic.edit import (
     CreateView,
     UpdateView,
 )
 from django.urls import reverse_lazy
-from django.shortcuts import render, redirect, get_object_or_404
 from .forms import TaskUpdateForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from django.views import View
-
 from .models import Task
 
 
